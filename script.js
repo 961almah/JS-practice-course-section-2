d0257e90e7f135f6f7f1aefc640de826e605e2c8
Ali Mahmoud <961alimahmoud@gmail.com>
2021-02-10 12:27:52 -0500

    functions assignment

--- script.js
+++ script.js
@@ -13,26 +13,39 @@
 
 // Functions //
 
-function logger() {
-    console.log(`My name is Ali`);
-}
+// function logger() {
+//     console.log(`My name is Ali`);
+// }
 
-logger();
+// logger();
 
-function fruitProcessor(apples, oranges) {
-    // console.log(apples, oranges);
-    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
-    return juice;
-}
+// function fruitProcessor(apples, oranges) {
+//     // console.log(apples, oranges);
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+//     return juice;
+// }
 
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
 
-const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
-
-
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
 
 // =============================================== //
 
+// Functions Assignment
+
+function describeCountry(country, population, capitalCity) {
+    const chosenCountry = `${country} has ${population} million people and its capital city is ${capitalCity}`;
+    return chosenCountry;
+}
+
+const lebanon = describeCountry(`Lebanon`, `5-6`, `Beirut`);
+console.log(lebanon);
+
+const italy = describeCountry(`Italy`, 60, `Roma`)
+console.log(italy);
+
+const USA = describeCountry(`USA`, 330, `DC`);
+console.log(USA);
 
