fe7a5d311b053062ebb21f8d21a9daa3523d5a3d
Ali Mahmoud <961alimahmoud@gmail.com>
2021-02-10 12:19:05 -0500

    functions

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
-'use strict';
+// 'use strict';
 
-let hasDriversLicense = false;
+// let hasDriversLicense = false;
 
-const passTest = true;
+// const passTest = true;
 
-if (passTest) hasDriversLicense = true;
+// if (passTest) hasDriversLicense = true;
 
 
-if (hasDriversLicense) console.log(`I can drive`);
+// if (hasDriversLicense) console.log(`I can drive`);
+
+// =============================================== //
+
+// Functions //
+
+function logger() {
+    console.log(`My name is Ali`);
+}
+
+logger();
+
+function fruitProcessor(apples, oranges) {
+    // console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+
+
+// =============================================== //
+
+
